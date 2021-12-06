10d624e84cf63c53bc734ed53d093d07dcbfb0ff
Todd Bleess <todd.bleess@gmail.com>
2021-12-06 09:47:39 -0700

    One chart

--- server.R
+++ server.R
@@ -9,51 +9,7 @@ county=reactive({filter(read.csv("county_names.csv"), county==input$county)%>%
                   select(countyfips)%>%
     as.numeric()})
 
-#### Population Estimates ####
-  output$estimates=renderPlotly({estimates_p(county())})
-  
-  est_data=reactive({estimates_d(county(), input$county)})
-  
-  output$estimatesData=downloadHandler(
-    filename= function(){
-      paste(unique(est_data()$County), paste0("Population Estimates 1985 to ", as.character(max(est_data()$year)), ".csv"))
-    },
-    content= function(file){
-      write.csv(est_data(), file, row.names=FALSE)
-    }
-    
-  )
-  
-### Net Migration by Age ####
-  
-  output$netMigAge=renderPlotly({mig_age_p(county())})
-  
-  nm_data=reactive({mig_age_d(county(), input$county)})
-  
-  output$netMigAgeData=downloadHandler(
-    filename= function(){
-      paste(unique(nm_data()$County), "Net Migration by Age.csv", sep="_")
-    },
-    content= function(file){
-      write.csv(nm_data(), file, row.names=FALSE)
-    }
-    
-  )
 
-  #### Population Projections ####
-  output$projections=renderPlotly({projections_p(county(), 2010)})
-  
-  proj_data=reactive({projections_d(county(), input$county, 2010)})
-  
-  output$projectionsData=downloadHandler(
-    filename= function(){
-      paste(unique(proj_data()$County), paste0("Population Projections 2010 to 2050.csv"))
-    },
-    content= function(file){
-      write.csv(proj_data(), file, row.names=FALSE)
-    }
-    
-  )  
   
 #### Components of Change ####
   output$components=renderPlotly({components_p(county())})
@@ -70,6 +26,4 @@ county=reactive({filter(read.csv("county_names.csv"), county==input$county)%>%
     
   )
   
-##### Percent Change By Age Group #####
-  output$projections_age=renderPlotly({projections_ageGroup(county())})
 }
--- ui.R
+++ ui.R
@@ -7,15 +7,10 @@ source("setup.R")
 function(req) {
   htmlTemplate("index.html",
                county=selectInput("county","Select a county:", choices = unique(county_choices$county), selected = 'Colorado'),
-               estimates_plot=plotlyOutput("estimates"),
-               estimates_dl=downloadButton('estimatesData', 'Download Data (CSV)'),
-               net_mig_plot=plotlyOutput("netMigAge"),
-               net_mig_dl=downloadButton('netMigAgeData', 'Download Data (CSV)'),
-               projections_plot=plotlyOutput("projections"),
-               projections_dl=downloadButton('projectionsData', 'Download Data (CSV)'),
+
                components_plot=plotlyOutput("components"),
                components_dl=downloadButton('componentsData', 'Download Data (CSV)'),
-               projections_age_plot=plotlyOutput("projections_age")
+
                )
 }
 
